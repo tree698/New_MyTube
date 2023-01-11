58ab69bc0efcd063b92e38934928918369a69935
Chanwoo <tree698@gmail.com>
2023-01-11 23:07:58 +0900

    add react query + display dummy videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SearchHeader from './components/SearchHeader';
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `${keyword}` : '🔥'}</div>;
+  const { isLoading, error, data: videos } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: async () =>
+      axios
+        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => {
+          console.log(res);
+          return res.data.items;
+        }),
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something went wrong!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
+// fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+//   .then((res) => res.json())
+//   .then((data) => data.items),
