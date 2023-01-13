021d54bd60b6efa295e58086966dfa9aba6f7d14
Chanwoo <tree698@gmail.com>
2023-01-13 14:41:31 +0900

    CSS: videoCard

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { formatTime } from '../util/date';
 
 export default function VideoCard({ video }) {
-  return <li>{video.snippet.title}</li>;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img className="w-full" src={thumbnails.medium.url} alt="" />
+      <div>
+        <p className="font-sembold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{formatTime(publishedAt, 'ko')}</p>
+      </div>
+    </li>
+  );
 }
